97d8a7392201d2904b9695f0994ac431a8ff9027
mojicamarcumplo <marco.mojica@ umplo.com>
2023-05-18 21:29:05 -0600

    Formulario Inventario

--- V2_TiendaBedu/app/src/main/java/org/bedu/v2_tiendabedu/InventarioFragment.kt
+++ V2_TiendaBedu/app/src/main/java/org/bedu/v2_tiendabedu/InventarioFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,17 +19,25 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class InventarioFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val spinner = view.findViewById<Spinner>(R.id.static_spinner)
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.type_model,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
         }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +45,9 @@ class InventarioFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inventario, container, false)
+
     }
+
 
     companion object {
         /**

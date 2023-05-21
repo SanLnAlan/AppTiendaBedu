b58b3bfb07046fffa22aad454ce7fcd9b71ffe0e
AlanSandoval <sandoval.leon.alan@gmail.com>
2023-05-21 00:35:03 -0600

    finalizacion catalogo - inicio carrito

--- V2_TiendaBedu/app/src/main/java/org/bedu/v2_tiendabedu/CarritoFragment.kt
+++ V2_TiendaBedu/app/src/main/java/org/bedu/v2_tiendabedu/CarritoFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,15 @@ class CarritoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_carrito, container, false)
+    }
+
+    private lateinit var buttonPagar : Button
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonPagar = view.findViewById(R.id.button_pagar)
+        buttonPagar.setOnClickListener {
+            Toast.makeText(context, "${orden.printListaProductos()}", Toast.LENGTH_SHORT)
+        }
     }
 
     companion object {

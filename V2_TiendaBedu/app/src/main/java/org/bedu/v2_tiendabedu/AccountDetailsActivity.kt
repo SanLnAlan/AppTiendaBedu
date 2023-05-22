4dd2f25500a34331ebfcb6f47a95ef76ac0cb00c
CarloMorale <carlosvulcan@hotmail.com>
2023-05-21 21:50:58 -0600

    Agrego DialogFragments para los botones de modificar apellido y modificar correo

--- V2_TiendaBedu/app/src/main/java/org/bedu/v2_tiendabedu/AccountDetailsActivity.kt
+++ V2_TiendaBedu/app/src/main/java/org/bedu/v2_tiendabedu/AccountDetailsActivity.kt
@@ -5,21 +5,27 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import org.bedu.v2_tiendabedu.EditEmailFragment.EditEmailDialogListener
 import org.bedu.v2_tiendabedu.EditNameFragment.EditNameDialogListener
+import org.bedu.v2_tiendabedu.EditLastnameFragment.EditLastnameDialogListener
 import org.bedu.v2_tiendabedu.models.user.arregloUsuarios
 
-class AccountDetailsActivity : AppCompatActivity(), EditNameDialogListener {
+class AccountDetailsActivity : AppCompatActivity(), EditNameDialogListener, EditLastnameDialogListener, EditEmailDialogListener {
 
     private lateinit var buttonName: ImageButton
+    private lateinit var buttonLastname: ImageButton
+    private lateinit var buttonEmail: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_details)
 
         buttonName = findViewById(R.id.nameButton)
+        buttonLastname = findViewById(R.id.lastnameButton)
+        buttonEmail = findViewById(R.id.emailButton)
 
-        buttonName.setOnClickListener {
-            showEditName()
-        }
+        buttonName.setOnClickListener {showEditName()}
+        buttonLastname.setOnClickListener{ showEditLastname()}
+        buttonEmail.setOnClickListener{ showEditEmail() }
     }
 
     private fun showEditName() {
@@ -28,12 +34,38 @@ class AccountDetailsActivity : AppCompatActivity(), EditNameDialogListener {
         editNameFragment.show(fragmentManager, "fragment_edit_name")
     }
 
+    private fun showEditLastname() {
+        val fragmentManager = supportFragmentManager
+        val editLastnameFragment = EditLastnameFragment.newInstance("title")
+        editLastnameFragment.show(fragmentManager, "fragment_edit_lastname")
+    }
+
+    private fun showEditEmail() {
+        val fragmentManager = supportFragmentManager
+        val editEmailFragment = EditEmailFragment.newInstance("title")
+        editEmailFragment.show(fragmentManager, "fragment_edit_email")
+    }
+
     // 3. This method is invoked in the activity when the listener is triggered
     // Access the data result passed to the activity here
-    override fun onFinishEditDialog(inputText: String) {
+    override fun onFinishEditDialogN(inputText: String) {
         val textName: TextView
         textName = findViewById(R.id.name)
         textName.text = inputText
-        Toast.makeText(this, "Hi, " + inputText, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Nombre cambiado correctamente", Toast.LENGTH_SHORT).show();
+    }
+
+    override fun onFinishEditDialogLn(inputText: String) {
+        val textName: TextView
+        textName = findViewById(R.id.lastname)
+        textName.text = inputText
+        Toast.makeText(this, "Apellido cambiado correctamente", Toast.LENGTH_SHORT).show();
+    }
+
+    override fun onFinishEditDialogE(inputText: String) {
+        val textName: TextView
+        textName = findViewById(R.id.email)
+        textName.text = inputText
+        Toast.makeText(this, "Correo cambiado exitosamente", Toast.LENGTH_SHORT).show();
     }
 }
--- V2_TiendaBedu/app/src/main/java/org/bedu/v2_tiendabedu/EditEmailFragment.java
+++ V2_TiendaBedu/app/src/main/java/org/bedu/v2_tiendabedu/EditEmailFragment.java
@@ -17,28 +17,26 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
 
 /**
  * A simple {@link DialogFragment} subclass.
- * Use the {@link EditNameFragment#newInstance} factory method to
+ * Use the {@link EditEmailFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EditNameFragment extends DialogFragment implements TextView.OnEditorActionListener {
+public class EditEmailFragment extends DialogFragment implements TextView.OnEditorActionListener {
     private EditText editText;
     private Button cancelButton;
     private Button acceptButton;
 
     // 1. Defines the listener interface with a method passing back data result.
-    public interface EditNameDialogListener {
-        void onFinishEditDialogN(String inputText);
+    public interface EditEmailDialogListener {
+        void onFinishEditDialogE(String inputText);
     }
 
-    public EditNameFragment(){ }
+    public EditEmailFragment(){ }
 
-    public static EditNameFragment newInstance(String title) {
-        EditNameFragment frag = new EditNameFragment();
+    public static EditEmailFragment newInstance(String title) {
+        EditEmailFragment frag = new EditEmailFragment();
         Bundle args = new Bundle();
         args.putString("title", title);
         frag.setArguments(args);
@@ -47,15 +45,15 @@ public class EditNameFragment extends DialogFragment implements TextView.OnEdito
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit_name, container);
+        return inflater.inflate(R.layout.fragment_edit_email, container);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         // Get field and buttons from view
-        editText = (EditText) view.findViewById(R.id.editNameField);
-        cancelButton = (Button) view.findViewById(R.id.cancelButton);
-        acceptButton = (Button) view.findViewById(R.id.acceptButton);
+        editText = (EditText) view.findViewById(R.id.editEmailField);
+        cancelButton = (Button) view.findViewById(R.id.cancelButtonE);
+        acceptButton = (Button) view.findViewById(R.id.acceptButtonE);
         // Fetch arguments from bundle and set title
         String title = getArguments().getString("title", "Ingrese su nombre");
         // Show soft keyboard automatically and request focus to field
@@ -65,9 +63,9 @@ public class EditNameFragment extends DialogFragment implements TextView.OnEdito
         editText.setOnEditorActionListener(this);
         cancelButton.setOnClickListener(v -> dismiss());
         acceptButton.setOnClickListener(v -> {
-                EditNameDialogListener listener = (EditNameDialogListener) getActivity();
-                listener.onFinishEditDialogN(editText.getText().toString());
-                dismiss();
+            EditEmailFragment.EditEmailDialogListener listener = (EditEmailFragment.EditEmailDialogListener) getActivity();
+            listener.onFinishEditDialogE(editText.getText().toString());
+            dismiss();
         });
     }
 
@@ -75,37 +73,11 @@ public class EditNameFragment extends DialogFragment implements TextView.OnEdito
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
         if(EditorInfo.IME_ACTION_DONE == actionId) {
             // Return input text back to activity through the implemented listener
-            EditNameDialogListener listener = (EditNameDialogListener) getActivity();
-            listener.onFinishEditDialogN(editText.getText().toString());
+            EditEmailFragment.EditEmailDialogListener listener = (EditEmailFragment.EditEmailDialogListener) getActivity();
+            listener.onFinishEditDialogE(editText.getText().toString());
             dismiss();
             return true;
         }
         return false;
     }
-
-    /*
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        String title = getArguments().getString("title");
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
-        alertDialogBuilder.setTitle(title);
-        alertDialogBuilder.setMessage("Are you sure?");
-        alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // on success
-            }
-        });
-        alertDialogBuilder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if(dialog != null) {
-                    dialog.dismiss();
-                }
-            }
-        });
-
-        return alertDialogBuilder.create();
-    }
-     */
 }
